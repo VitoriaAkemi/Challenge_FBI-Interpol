227a55df58665e8c7fd35927f225da67df0efe9b
Vitoria Akemi <vitoriaakemitanimura@gmail.com>
2023-05-11 16:18:55 -0300

    projeto base pelo spring initializer e configurações do banco

---
+++ FBI-Interpol/src/main/java/br/com/fiap/FBIInterpol/FbiInterpolApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.FBIInterpol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FbiInterpolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FbiInterpolApplication.class, args);
+	}
+
+}
